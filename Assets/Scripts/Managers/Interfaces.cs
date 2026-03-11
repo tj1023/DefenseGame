a474b0891a5437535640e5000251824611920ffe
tj1023 <htjeong1023@naver.com>
2026-03-11 23:08:25 +0900

    chore: 자잘한 것들 정리

--- Assets/Scripts/Managers/Interfaces.cs
+++ Assets/Scripts/Managers/Interfaces.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 namespace Managers
 {
     /// <summary>
+    /// InputManager에 의해 감지되어 터치/클릭이 가능한 객체가 구현해야 할 인터페이스
+    /// </summary>
+    public interface IClickable
+    {
+        void OnClick();
+    }
+    
+    /// <summary>
     /// InputManager에 의해 감지되어 터치/마우스 드래그가 가능한 객체가 구현해야 할 인터페이스
     /// </summary>
     public interface IDraggable
@@ -10,13 +18,5 @@ namespace Managers
         void OnDragStart(Vector2 position);
         void OnDrag(Vector2 position);
         void OnDragEnd(Vector2 position);
-    }
-
-    /// <summary>
-    /// InputManager에 의해 감지되어 터치/클릭이 가능한 객체가 구현해야 할 인터페이스
-    /// </summary>
-    public interface IClickable
-    {
-        void OnClick();
     }
 }
